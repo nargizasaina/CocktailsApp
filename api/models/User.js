f566897bde3f0d1194a7a9535f4b98d2555f8e3f
Nargiza Sainazarova <nargizasaina@gmail.com>
2022-10-26 14:02:12 +0600

    Added User model

---
+++ api/models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const {nanoid} = require("nanoid");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        required: true,
+        unique: true,
+        type: String,
+        validate: {
+            validator: async value => {
+                const user = await User.findOne({email: value});
+                if (user) return false;
+            },
+            message: 'The user is already registered!',
+        },
+    },
+    googleId: {
+        required: true,
+        type: String
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    displayName: {
+        type: String,
+        required: true
+    },
+    role: {
+        required: true,
+        type: String,
+        default: 'user',
+        enum: ['admin', 'user']
+    },
+    avatarImage: String
+});
+
+UserSchema.methods.generateToken = function () {
+    this.token = nanoid();
+};
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
