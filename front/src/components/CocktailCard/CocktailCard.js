4a6d4c51642ef2a8b4ae7003c6aefe5441e336b2
Nargiza Sainazarova <nargizasaina@gmail.com>
2022-10-28 14:13:36 +0600

    Added preloader

--- front/src/components/CocktailCard/CocktailCard.js
+++ front/src/components/CocktailCard/CocktailCard.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Box, Button, Card, CardContent, CardMedia, Typography} from "@mui/material";
 import {useDispatch, useSelector} from "react-redux";
 import {deleteCocktailRequest, publishCocktailRequest} from "../../store/actions/cocktailsActions";
+import Spinner from "../UI/Spinner/Spinner";
 
 const CocktailCard = (props) => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.users.user);
+    const loading = useSelector(state => state.cocktails.loading);
 
     const onPublish = id => {
         dispatch(publishCocktailRequest(id));
@@ -15,80 +17,82 @@ const CocktailCard = (props) => {
         dispatch(deleteCocktailRequest(id));
     };
 
-    return props.cocktail && (
-        <Box maxWidth="700px" margin="0 auto">
-            <Card>
-                <Box sx={{ display: 'flex' }}>
-                    <CardMedia
-                        component="img"
-                        sx={{ width: 250 }}
-                        image={props.cocktail.image}
-                        alt="Cocktail"
-                    />
-                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                        <CardContent sx={{ flex: '1 0 auto' }}>
-                            <Typography component="div" variant="h5">
-                                <strong>{props.cocktail.title}</strong>
-                            </Typography>
-                            <Typography variant="subtitle1" color="text.primary" component="div" marginTop="10px">
-                                <b>Ingredients</b>
-                            </Typography>
-                            <ul style={{padding: '0 20px', fontSize: '14px', margin:0}}>
-                                {props.cocktail.ingredients.map(ing => (
-                                    <li key={ing._id}>
-                                        {ing.title}: {ing.amount}
-                                    </li>
-                                ))}
-                            </ul>
-                        </CardContent>
-                    </Box>
-                    {user?.role === 'admin' &&
-                        <Box alignSelf="flex-start" marginLeft="auto" marginRight="5px">
-                            {!props.cocktail.publish &&
-                                <>
-                                    <Typography variant="subtitle1" color="text.secondary" component="div" marginTop="10px">
-                                        Unpublished!
-                                    </Typography>
+    return loading
+            ? <Spinner/>
+            : props.cocktail && (
+            <Box maxWidth="700px" margin="0 auto">
+                <Card>
+                    <Box sx={{ display: 'flex' }}>
+                        <CardMedia
+                            component="img"
+                            sx={{ width: 250 }}
+                            image={props.cocktail.image}
+                            alt="Cocktail"
+                        />
+                        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                            <CardContent sx={{ flex: '1 0 auto' }}>
+                                <Typography component="div" variant="h5">
+                                    <strong>{props.cocktail.title}</strong>
+                                </Typography>
+                                <Typography variant="subtitle1" color="text.primary" component="div" marginTop="10px">
+                                    <b>Ingredients</b>
+                                </Typography>
+                                <ul style={{padding: '0 20px', fontSize: '14px', margin:0}}>
+                                    {props.cocktail.ingredients.map(ing => (
+                                        <li key={ing._id}>
+                                            {ing.title}: {ing.amount}
+                                        </li>
+                                    ))}
+                                </ul>
+                            </CardContent>
+                        </Box>
+                        {user?.role === 'admin' &&
+                            <Box alignSelf="flex-start" marginLeft="auto" marginRight="5px">
+                                {!props.cocktail.publish &&
+                                    <>
+                                        <Typography variant="subtitle1" color="text.secondary" component="div" marginTop="10px">
+                                            Unpublished!
+                                        </Typography>
+                                        <Button
+                                            fullWidth
+                                            type="button"
+                                            color="success"
+                                            variant="contained"
+                                            onClick={() => onPublish(props.cocktail._id)}
+                                        >
+                                            Publish
+                                        </Button>
+                                    </>
+                                }
+                                <Box marginTop="15px">
                                     <Button
                                         fullWidth
                                         type="button"
-                                        color="success"
+                                        color="error"
                                         variant="contained"
-                                        onClick={() => onPublish(props.cocktail._id)}
+                                        onClick={() => onDelete(props.cocktail._id)}
                                     >
-                                        Publish
+                                        Delete
                                     </Button>
-                                </>
-                            }
-                            <Box marginTop="15px">
-                                <Button
-                                    fullWidth
-                                    type="button"
-                                    color="error"
-                                    variant="contained"
-                                    onClick={() => onDelete(props.cocktail._id)}
-                                >
-                                    Delete
-                                </Button>
+                                </Box>
                             </Box>
-                        </Box>
-                    }
-                </Box>
-                <Box padding="5px">
-                    <Typography variant="subtitle1" color="text.primary" component="div" marginTop="10px">
-                        <b>Recipe</b>
-                    </Typography>
-                    <p style={{fontSize: '14px', margin: 0}}>
-                        {props.cocktail.recipe}
-                    </p>
-                    <Typography variant="subtitle1" color="text.primary" component="div" marginTop="10px">
-                        <b>
-                            Rating: {props.cocktail.ratings.reduce((prev, curr) => (prev.rating + curr.rating) / props.cocktail.ratings.length)}
-                        </b> ({props.cocktail.ratings.length} votes)
-                    </Typography>
-                </Box>
-            </Card>
-        </Box>
+                        }
+                    </Box>
+                    <Box padding="5px">
+                        <Typography variant="subtitle1" color="text.primary" component="div" marginTop="10px">
+                            <b>Recipe</b>
+                        </Typography>
+                        <p style={{fontSize: '14px', margin: 0}}>
+                            {props.cocktail.recipe}
+                        </p>
+                        <Typography variant="subtitle1" color="text.primary" component="div" marginTop="10px">
+                            <b>
+                                Rating: {props.cocktail.ratings.reduce((prev, curr) => (prev.rating + curr.rating) / props.cocktail.ratings.length)}
+                            </b> ({props.cocktail.ratings.length} votes)
+                        </Typography>
+                    </Box>
+                </Card>
+            </Box>
     );
 };
 
--- front/src/containers/CocktailBuilder/CocktailBuilder.js
+++ front/src/containers/CocktailBuilder/CocktailBuilder.js
@@ -3,10 +3,12 @@ import {Box, Card, CardActionArea, CardContent, CardMedia, Typography} from "@mu
 import {useDispatch, useSelector} from "react-redux";
 import {fetchAllCocktailsRequest} from "../../store/actions/cocktailsActions";
 import {Link} from "react-router-dom";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 const CocktailBuilder = () => {
     const dispatch = useDispatch();
     const cocktails = useSelector(state => state.cocktails.allCocktails);
+    const loading = useSelector(state => state.cocktails.loading);
     const user = useSelector(state => state.users.user);
 
     useEffect(() => {
@@ -14,31 +16,35 @@ const CocktailBuilder = () => {
     }, [dispatch]);
 
     return (
-        <Box display="flex" justifyContent="space-evenly">
-            {cocktails.map(cocktail => (
-                (((user?.role === 'user' && cocktail.publish) || user?.role === 'admin') &&
-                <Card sx={{ maxWidth: 255, margin: '5px' }} key={cocktail._id}>
-                    <CardActionArea component={Link} to={'/cocktails/' + cocktail._id}>
-                        <CardMedia
-                            component="img"
-                            height="240"
-                            image={cocktail.image}
-                            alt="cocktail"
-                        />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {cocktail.title}
-                            </Typography>
-                            {user?.role === 'admin' && !cocktail.publish &&
-                                <Typography sx={{ fontSize: 12 }} variant="body2" color="text.secondary">
-                                    Unpublished yet!
-                                </Typography>
-                            }
-                        </CardContent>
-                    </CardActionArea>
-                </Card> )
-            ))}
-        </Box>
+        loading
+            ? <Spinner/>
+            : <Box display="flex" justifyContent="space-evenly">
+                {cocktails && cocktails.length > 1
+                ? cocktails.map(cocktail => (
+                    (((user?.role === 'user' && cocktail.publish) || user?.role === 'admin') &&
+                        <Card sx={{maxWidth: 255, margin: '5px'}} key={cocktail._id}>
+                            <CardActionArea component={Link} to={'/cocktails/' + cocktail._id}>
+                                <CardMedia
+                                    component="img"
+                                    height="240"
+                                    image={cocktail.image}
+                                    alt="cocktail"
+                                />
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div">
+                                        {cocktail.title}
+                                    </Typography>
+                                    {user?.role === 'admin' && !cocktail.publish &&
+                                        <Typography sx={{fontSize: 12}} variant="body2" color="text.secondary">
+                                            Unpublished yet!
+                                        </Typography>
+                                    }
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>)
+                ))
+                : <h2>There are no any cocktails. Please add...</h2>}
+            </Box>
     );
 };
 
